bb89cc036f6b55953012215343255c10b27aa891
MoviesHuntHD <68388096+MoviesHuntHD@users.noreply.github.com>
2022-06-07 21:17:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MoviesHuntHD/ArchivesFilesBot /EvaMaria
+  git clone https://github.com/MoviesHuntHD/ArchivesFilesBot /Akshay
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Akshay
 fi
-cd /EvaMaria
+cd /Akshay
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
