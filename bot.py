d6557bcea0b5793758bc74ef3ee2de5cfd311563
akshaybangad5 <75175325+akshaybangad5@users.noreply.github.com>
2022-11-27 16:07:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,6 @@ from utils import temp
 class Bot(Client):
 
     def __init__(self):
-        super().__init__(
             session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
